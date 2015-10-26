6f4972f567dba47605a902586308379495a190dd
Mikołaj Szczepkowski <mikolajszczepkowski@gmail.com>
2015-10-26 21:29:17 +0100

    change container and more

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 $(function (){
-	var gameImageHeight = $("#gameImage").height();
+	var gameImageHeight = $("#gameImage").height(),
+		width = $(document).width() - 2;
 
 	$(".decoration-bar").css("height", gameImageHeight);
 	
 	$(window).resize(function(){
 		gameImageHeight = $("#gameImage").height();
+		width = $(document).width() - 2;
 		$(".decoration-bar").css("height", gameImageHeight);
+	});
+
+	$("#chat").on("click", function(){
+		$("#friends").slideToggle(1000);
 	});
 });
