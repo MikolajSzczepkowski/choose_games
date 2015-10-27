8ab64a76aa22f40e4b73b5def6b971b3e4c16db4
Mikołaj Szczepkowski <mikolajszczepkowski@gmail.com>
2015-10-27 13:48:54 +0100

    bigger menu buttons for mobile

--- app.js
+++ app.js
@@ -2,8 +2,7 @@ $(function (){
 	var gameImageHeight = $(".game-image").height(),
 		width = $(window).width(),
 		friendsHeight = $("#friends").height() + 10 +"px",
-		toggleCounter = 0,
-		gameImageCounter = 0;
+		toggleCounter = 0;
 
 	$(".decoration-bar").css("height", gameImageHeight);
 	
@@ -30,20 +29,9 @@ $(function (){
 		},800);
 		toggleCounter ++;
 	});
+	
 	$(".game-image").on("mouseenter", function(){
 		$(this).prepend("<div class='game-image-hover' data-active=active></div>");
-		var ifActive = $(".game-image-hover").attr("data-active");
-		// if (ifActive === "active") {
-		// }
-		// else{
-		// 	$(".game-image").on("click", function(){
-		// 		$(this).prepend("<div class='game-image-hover'></div>");
-		// 		gameImageCounter ++;
-		// 		if (gameImageCounter%2 === 0) {
-		// 			$(".game-image-hover").remove();
-		// 		}
-		// 	});
-		// }
 	});
 	$(".game-image").on("mouseleave", function(){
 		$(".game-image-hover").remove();
