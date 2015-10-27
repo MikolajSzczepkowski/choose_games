817b7930dade57d188cc3dfd2a627aae12bf3507
Mikołaj Szczepkowski <mikolajszczepkowski@gmail.com>
2015-10-27 12:22:57 +0100

    friends bobrder

--- app.js
+++ app.js
@@ -16,14 +16,11 @@ $(function (){
 		width = $(window).width(),
 		friendsHeight = $("#friends").height() + 10 +"px";
 		if(width <= 990){
-			console.log(toggleCounter);
 			if(toggleCounter%2 === 0){
 				$(".main").animate({paddingTop: friendsHeight});
 			}
 			else{
 				$(".main").animate({paddingTop: "0px"});
-							console.log("weszlo i tu");
-
 			}
 		}
 		$("#friends").toggle( "slide", { 
