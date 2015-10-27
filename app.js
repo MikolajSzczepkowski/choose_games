f369dab813fbd109cc1e180d31c01d6dd18ec8db
Mikołaj Szczepkowski <mikolajszczepkowski@gmail.com>
2015-10-27 14:33:42 +0100

    game hovers

--- app.js
+++ app.js
@@ -29,9 +29,16 @@ $(function (){
 		},800);
 		toggleCounter ++;
 	});
-	
+
 	$(".game-image").on("mouseenter", function(){
-		$(this).prepend("<div class='game-image-hover' data-active=active></div>");
+		$(this).prepend("<div class='game-image-hover'></div>");
+		$(".game-image-hover").append("<p class='text-center text-uppercase game-hover-players'></p>");
+		$(".game-image-hover").append("<p class='text-center text-uppercase game-hover-info'></p>");
+		$(".game-image-hover").append("<p class='text-center text-uppercase game-hover-fee'></p>");
+		$(".game-hover-players").html("players<br>24/64");
+		$(".game-hover-info").html("Registartion ends<br> 28.10.2015");
+		$(".game-hover-fee").html("678$");
+		
 	});
 	$(".game-image").on("mouseleave", function(){
 		$(".game-image-hover").remove();
