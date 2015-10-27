aa564e9e09ef28bc24cc662b4c3e3b81e0851c72
Mikołaj Szczepkowski <mikolajszczepkowski@gmail.com>
2015-10-27 11:19:25 +0100

    right chat box aniamtion

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 $(function (){
 	var gameImageHeight = $("#gameImage").height(),
-		width = $(document).width() - 2;
+		width = $(document).width() - 2,
+		friendsHeight = $("#friends").height() + 10 +"px"
+		toggleCounter = 0;
 
 	$(".decoration-bar").css("height", gameImageHeight);
 	
@@ -11,6 +13,17 @@ $(function (){
 	});
 
 	$("#chat").on("click", function(){
-		$("#friends").slideToggle(1000);
+		width = $(document).width() - 2;
+		if(width <= 768){
+			if(toggleCounter%2 === 0){
+				$(".main").animate({paddingTop: friendsHeight});
+			}
+			else{
+				$(".main").animate({paddingTop: "0px"});
+			}
+			toggleCounter ++;
+		}
+		$("#friends").toggle( "slide", { direction: "right", 
+											easing: "easeInOutBack" },800);
 	});
 });
