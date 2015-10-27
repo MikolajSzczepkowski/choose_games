e4c33c3e8d75eeaf7726a93ee3987882e9fdf2a9
Mikołaj Szczepkowski <mikolajszczepkowski@gmail.com>
2015-10-27 12:14:45 +0100

    reponsive chat box

--- app.js
+++ app.js
@@ -1,29 +1,35 @@
 $(function (){
-	var gameImageHeight = $("#gameImage").height(),
-		width = $(document).width() - 2,
-		friendsHeight = $("#friends").height() + 10 +"px"
+	var gameImageHeight = $(".game-image").height(),
+		width = $(window).width(),
+		friendsHeight = $("#friends").height() + 10 +"px",
 		toggleCounter = 0;
 
 	$(".decoration-bar").css("height", gameImageHeight);
 	
 	$(window).resize(function(){
-		gameImageHeight = $("#gameImage").height();
-		width = $(document).width() - 2;
+		gameImageHeight = $(".game-image").height();
+		width = $(window).width();
 		$(".decoration-bar").css("height", gameImageHeight);
 	});
 
 	$("#chat").on("click", function(){
-		width = $(document).width() - 2;
-		if(width <= 768){
+		width = $(window).width(),
+		friendsHeight = $("#friends").height() + 10 +"px";
+		if(width <= 990){
+			console.log(toggleCounter);
 			if(toggleCounter%2 === 0){
 				$(".main").animate({paddingTop: friendsHeight});
 			}
 			else{
 				$(".main").animate({paddingTop: "0px"});
+							console.log("weszlo i tu");
+
 			}
-			toggleCounter ++;
 		}
-		$("#friends").toggle( "slide", { direction: "right", 
-											easing: "easeInOutBack" },800);
+		$("#friends").toggle( "slide", { 
+			direction: "right", 
+			easing: "easeInOutBack" 
+		},800);
+		toggleCounter ++;
 	});
 });
