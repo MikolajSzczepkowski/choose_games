020f981fa3a02fa3372acab3f47ea6d3eeb6fd45
Mikołaj Szczepkowski <mikolajszczepkowski@gmail.com>
2015-10-29 13:41:49 +0100

    ver 2 improvements

--- app.js
+++ app.js
@@ -1,22 +1,29 @@
 $(function (){
-	var gameImageHeight = $(".game-image").height(),
-		width = $(window).width(),
-		friendsHeight = $("#friends").height() + 10 +"px",
-		toggleCounter = 0;
+	var pageHeight = $(document).height(),
+		pageWidth = $(document).width();
 
-	$(".decoration-bar").css("height", gameImageHeight);
-	
+	if(pageWidth>=990){
+		$(".friends").css("height", pageHeight);
+	}
+	else{
+		$(".friends").css("height", "auto");
+	}
 	$(window).resize(function(){
-		gameImageHeight = $(".game-image").height();
-		width = $(window).width();
-		$(".decoration-bar").css("height", gameImageHeight);
+		pageHeight = $(document).height(),
+    	pageWidth = $(document).width();
+    	if(pageWidth>=990){
+			$(".friends").css("height", pageHeight);
+		}
+		else{
+			$(".friends").css("height", "auto");
+		}
 	});
 
 	$("#chat").on("click", function(){
 		$("#friends").toggle( "slide", { 
-			direction: "right", 
-			easing: "easeInOutBack" 
-		},700);
+			direction: "right",
+			easing: "swing"
+		},500);
 	});
 
 	$(".game-image").on("mouseenter", function(){
@@ -25,7 +32,7 @@ $(function (){
 		$(".game-image-hover").append("<p class='text-center text-uppercase game-hover-info'></p>");
 		$(".game-image-hover").append("<p class='text-center text-uppercase game-hover-fee'></p>");
 		$(".game-hover-players").html("players<br>24/64");
-		$(".game-hover-info").html("Registartion ends<br> 28.10.2015");
+		$(".game-hover-info").html("Registration ends<br> 28.10.2015");
 		$(".game-hover-fee").html("678$");
 		
 	});
