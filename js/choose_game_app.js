66dc3616c15ff3b21c82d5bc3abebc889a0e8866
Mikołaj Szczepkowski <mikolajszczepkowski@gmail.com>
2016-02-03 14:55:27 +0100

    show more functionality

--- js/choose_game_app.js
+++ js/choose_game_app.js
@@ -11,4 +11,17 @@ $(function (){
 	$(".game-image").on("mouseleave", function(){
 		$(".game-image-hover").remove();
 	});
+
+	$("#latestMatches li:lt(3)").show();
+	$(document).on("click", "#showMoreMatches", function(){
+		$("#latestMatches li").show();
+		$(this).text("hide");
+		$(this).attr("id", "hideMatches");
+	});
+	$(document).on("click", "#hideMatches", function(){
+		$("#latestMatches li").hide();
+		$("#latestMatches li:lt(3)").show();
+		$(this).text("show more");
+		$(this).attr("id", "showMoreMatches");
+	});
 });
